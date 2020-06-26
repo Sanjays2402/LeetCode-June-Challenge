75068dfe73fc6a06017f14b1538ee0b79e61ff75
Sanjay <sanjays2402@gmail.com>
2020-06-27 00:16:07 +0530

    Create June 16 Solution.cpp

---
+++ June 26/June 16 Solution.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        return sum(root,0);
+    }
+    
+    int sum(TreeNode* root, int su)
+    {
+        if(!root)
+            return 0;
+        int cur_node = su*10 + root->val;
+        if(root->left==0 && root->right==0)
+            return cur_node;
+        return sum(root->left,cur_node) + sum(root->right,cur_node);
+        
+    }
+};
